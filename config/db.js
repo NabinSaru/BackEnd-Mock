1e9811c9c473df49cf50ad2a0ba37ce4c8001c3f
Nabin Saru <jenexthegreat@gmail.com>
2025-09-06 19:18:51 +0545

    feat: implement the mongoose db handling

---
+++ config/db.js
@@ -0,0 +1,14 @@
+// db.js
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.mongo_connection_string);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
