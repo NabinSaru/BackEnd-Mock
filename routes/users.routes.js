8b29d664a369b1a7170a3d7d94428207c7ef55e2
Nabin Saru <jenexthegreat@gmail.com>
2025-09-06 20:18:19 +0545

    feat: add routes for the user controllers

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { addUser, getUsers } = require('../controllers/users');
+const { addUser, getUsers, updateUser, deleteUser } = require('../controllers/users');
 
 router.post('/', addUser);
 router.get('/', getUsers);
+router.get('/:id', updateUser);
+router.get('/:id', deleteUser);
 
 module.exports = router;
